edcb1b651307a37f12a7331602a597529dc1a728
alecchung <aleclavie@proton.me>
2023-03-16 12:21:14 +0800

    sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   Box,
+  Divider,
   Drawer,
   IconButton,
   List,
@@ -25,10 +26,12 @@ import {
   AdminPanelSettingsOutlined,
   TrendingUpOutlined,
   PieChartOutlined,
+  SettingsOutlined,
 } from "@mui/icons-material";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import FlexBetween from "./FlexBetween";
+import profileImage from 'assets/profile.png'
 
 const navItems = [
   {
@@ -121,10 +124,12 @@ const Sidebar = ({
               boxSixing: "border-box",
               borderWidth: isNonMobile ? 0 : "2px",
               width: drawerWidth,
+              display: "grid",
+              justifyContent: "space-between"
             },
           }}
         >
-          <Box width="100%">
+          <Box width="100%" display="flex" flexDirection="column" justifyContent="space-between">
             <Box m="1.5rem 2rem 2rem 3rem">
               <FlexBetween color={theme.palette.secondary.main}>
                 <Box display="flex" alignItems="center" gap="0.5rem">
@@ -188,46 +193,49 @@ const Sidebar = ({
                 );
               })}
             </List>
+            <Box>
+              <Divider/>
+              <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 1rem 3rem">
+                <Box
+                  component="img"
+                  alt="profile"
+                  src={profileImage}
+                  height="40px"
+                  width="40px"
+                  borderRadius="50%"
+                  sx={{ objectFit: "cover" }}
+                />
+                <Box textAlign="left">
+                  <Typography
+                    fontWeight="bold"
+                    fontSize="0.9rem"
+                    sx={{ color: theme.palette.secondary[100] }}
+                  >
+                    {user.name}
+                  </Typography>
+                  <Typography
+                    fontSize="0.8rem"
+                    sx={{ color: theme.palette.secondary[200] }}
+                  >
+                    {user.occupation}
+                  </Typography>
+                </Box>
+                <SettingsOutlined
+                  sx={{
+                    color: theme.palette.secondary[300],
+                    fontSize: "30px ",
+                    cursor: "pointer"
+                  }}
+                />
+              </FlexBetween>
+            </Box>
           </Box>
 
-          {/* <Divider />
-          <Box position="absolute" bottom="2rem">
-            <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
-              <Box
-                component="img"
-                alt="profile"
-                src={profileImage}
-                height="40px"
-                width="40px"
-                borderRadius="50%"
-                sx={{ objectFit: "cover" }}
-              />
-              <Box textAlign="left">
-                <Typography
-                  fontWeight="bold"
-                  fontSize="0.9rem"
-                  sx={{ color: theme.palette.secondary[100] }}
-                >
-                  {user.name}
-                </Typography>
-                <Typography
-                  fontSize="0.8rem"
-                  sx={{ color: theme.palette.secondary[200] }}
-                >
-                  {user.occupation}
-                </Typography>
-              </Box>
-              <SettingsOutlined
-                sx={{
-                  color: theme.palette.secondary[300],
-                  fontSize: "25px ",
-                }}
-              />
-            </FlexBetween>
-          </Box> */}
+
         </Drawer>
       )}
-    </Box>
+
+    </Box >
   );
 };
 
